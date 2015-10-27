035c61e7be070f09aaadc83ffcbd1cea170ea291
Ludwig <ludwig@LudwigdeMac.local>
2015-10-27 14:22:10 +0800

    Initial Commit

---
+++ ToDoListApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToDoListApp
+//
+//  Created by Ludwig on 15/10/27.
+//  Copyright (c) 2015年 Ludwig. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
